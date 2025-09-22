eaa42cf9446f16ab015b67859f2e47255bc13b1c
bluewolf113 <57601736+bluewolf113@users.noreply.github.com>
2025-09-22 12:25:34 -0400

    Create sh_antlionlegcooked.lua

---
+++ ixdreams/schema/items/food/sh_antlionlegcooked.lua
@@ -0,0 +1,13 @@
+ITEM.base = "base_food"
+
+ITEM.name = "Cooked Antlion Leg"
+ITEM.model = Model("models/props/cryts_food/fruit_apple.mdl")
+ITEM.width = 3
+ITEM.height = 1
+ITEM.description = "The chitinous outside has been blackened, while the meat inside has been made tender and juicy."
+ITEM.eat = "."
+ITEM.category = "Food"
+ITEM.hunger = 6
+ITEM.thirst = 3
+ITEM.nutrition = 30
+ITEM.uses = 4
